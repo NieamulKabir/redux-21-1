cc4317d5bc1c66e7dde2f4b351acefef0ff119ba
NieamulKabir <kabir000079@gmail.com>
2024-01-20 21:35:12 +0600

    counter update

--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -25,7 +25,7 @@ export const counterSlice = createSlice({
   },
 })
 
-// Action creators are generated for each case reducer function
+
 export const { increment, decrement, incrementByAmount } = counterSlice.actions
 
 export default counterSlice.reducer
